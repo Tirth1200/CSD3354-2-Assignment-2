eddbc8de66cfd7c328f29bbff84a39e13303047b
visualstudio <visualstudio@122-PC04>
2019-03-08 21:19:28 -0500

    Lab_13

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -10,63 +10,36 @@ using System.Threading.Tasks;
 namespace DelegatesAndEvents
 {
     using System;
-    delegate void ExampleDelegate(string xyz);
+    
     class Program
     {
-        public static void Method1(string xyz)
-
+        public static void Main(string[] args)
         {
-
-            Console.WriteLine(xyz + " Method1");
-
-        }
-
-        public static void Method2(string xyz)
-
-        {
-
-            Console.WriteLine(xyz + " Method2");
-
-        }
-
-        public static void Main()
-
-        {
-
-            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
-
-            ex1Delegate = new ExampleDelegate(Method1);
-
-            ex2Delegate = new ExampleDelegate(Method2);
-
-            ex3Delegate = ex1Delegate + ex2Delegate;
-
-            myDelegate = ex1Delegate - ex2Delegate;
-
-            ex1Delegate("AAA");
-
-       
-
-            ex2Delegate("BBB");
-
-            ex3Delegate("CCC");
-
-            myDelegate("DDD");
-
-            myDelegate = ex3Delegate - ex1Delegate;
-
-            myDelegate("EEE");
-
-            myDelegate = ex3Delegate - ex2Delegate;
-
-            myDelegate("FFF");
-
+            DelegateExercises de = new DelegateExercises();
+            de.Method3();
             Console.ReadLine();
-
-
-
         }
     }
+    delegate int MyDelegate(out int i);
+    public class DelegateExercises
+    {
+        int Method1(out int i)
+        {
+            Console.WriteLine("Method 1");
+            i = 10;
+            return 0;
+        }
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            MyDelegate myDelegate1 = new MyDelegate(Method1);
+            MyDelegate myDelegate2 = myDelegate + myDelegate1;
+            int intValue;
+            myDelegate2(out intValue);
+        }
+
 
     }
+
+}
 
