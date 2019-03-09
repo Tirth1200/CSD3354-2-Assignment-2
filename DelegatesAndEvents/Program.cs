7e8c546d3926c16238e72a2c3f6bb34606ca3ced
visualstudio <visualstudio@122-PC04>
2019-03-08 19:34:17 -0500

    Lab05

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -14,30 +14,22 @@ namespace DelegatesAndEvents
         static void Main(string[] args)
         {
             DelegateExercises de = new DelegateExercises();
-            de.Method3(10);
+            de.Method3();
             Console.Read();
         }
     }
 
     public class DelegateExercises
     {
-        public delegate int MyDelegate(int i);
-        public int Method1(int intMethod1)
+        public delegate void MyDelegate();
+        void Method1()
         {
-            return intMethod1 * 2;
+            System.Console.WriteLine("MyDelegate");
         }
-        public int Method2(int intMethod2)
-        {
-            return intMethod2 * 10;
-        }
-        public void Method3(int intMethod3)
+        public void Method3()
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            int result1 = myDelegate(10);
-            System.Console.WriteLine(result1);
-            myDelegate = new MyDelegate(Method2);
-            int result2 = myDelegate(10);
-            System.Console.WriteLine(result2);
+            myDelegate();
         }
     }
 }
